2b46ffeaf340d381eff06a61428a7eafbf39c687
Martha Sharpe <marthasharpe2020@gmail.com>
2019-11-23 18:37:06 -0500

    indicators and controls do not overlap cards

--- src/components/cardCarousel/CardCarousel.js
+++ src/components/cardCarousel/CardCarousel.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import {
+    // Row,
+    // Col,
     Carousel,
     CarouselItem,
     CarouselControl,
@@ -12,20 +14,62 @@ import {
     CardFooter,
 } from 'reactstrap';
 import { connect } from 'react-redux';
+import './CardCarousel.scss';
+
 
 const CardCarousel = (props) => {
     const [activeIndex, setActiveIndex] = useState(0);
     const [animating, setAnimating] = useState(false);
-    
+ 
+    // map data onto cards
+    const cards = props.data.map((obj) => {
+        return (
+            <CarouselItem
+                onExiting={() => setAnimating(true)}
+                onExited={() => setAnimating(false)}
+                key={obj.Heading}
+            >
+                {/* <Col> */}
+                    <Card className="shadow rounded">
+                        {/* display image if in json data */}
+                        {obj.showBridge ? 
+                            <CardImg top height="150px" width="100%" src="https://ih0.redbubble.net/image.9487584.0600/pp,550x550.jpg" alt="golden gate bridge"/>
+                            :
+                            <CardImg top height="150px" width="100%" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbmDSPoVhPaWkpmlsCFKh4HTbEjnobbuks8SJBAkUA7a7RZEGr&s" alt="black background"/>}
+                        <CardBody>
+                            <CardTitle className="text-capitalize">
+                                {obj.Heading}
+                            </CardTitle>
+                            <CardSubtitle className="text-capitalize">
+                                <small>{obj.Subheading}</small>
+                            </CardSubtitle>
+                        </CardBody>
+                        <CardFooter className="text-success text-right">
+                            ${obj.Price.toString().replace(/(\d+)(?=(\d{3}))/g, '$1,')}
+                        </CardFooter>
+                    </Card>
+                {/* </Col> */}
+            </CarouselItem>
+        );
+    });
+
+    // split cards into sets
+    let cardSets = [];   
+    for (let i = 0; i < cards.length; i += 3) {
+        let cardSet = cards.slice(i, i + 3);
+        cardSets.push(cardSet);
+    }
+    console.log(cardSets);
+
     const next = () => {
         if (animating) return;
-        const nextIndex = activeIndex === props.data.length - 1 ? 0 : activeIndex + 1;
+        const nextIndex = activeIndex === cards.length - 1 ? 0 : activeIndex + 1;
         setActiveIndex(nextIndex);
     }
 
     const previous = () => {
         if (animating) return;
-        const nextIndex = activeIndex === 0 ? props.data.length - 1 : activeIndex - 1;
+        const nextIndex = activeIndex === 0 ? cards.length - 1 : activeIndex - 1;
         setActiveIndex(nextIndex);
     }
 
@@ -34,54 +78,21 @@ const CardCarousel = (props) => {
         setActiveIndex(newIndex);
     }
 
-    //map data onto cards
-    const cards = props.data.map((item) => {
-        return (
-          <CarouselItem
-            onExiting={() => setAnimating(true)}
-            onExited={() => setAnimating(false)}
-            key={item.Heading}
-          >
-                <Card
-                    style={{width:"200px", height:"100%"}}
-                    className="shadow rounded"
-                >
-                    {item.showBridge ? <CardImg top height="150px" width="100%" src="https://ih0.redbubble.net/image.9487584.0600/pp,550x550.jpg" alt="golden gate bridge"/> : <CardImg top height="150px" width="100%" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbmDSPoVhPaWkpmlsCFKh4HTbEjnobbuks8SJBAkUA7a7RZEGr&s" alt="black background"/>}
-                    <CardBody>
-                        <CardTitle
-                            className="text-capitalize"
-                        >
-                        {item.Heading}
-                        </CardTitle>
-                        <CardSubtitle
-                            className="text-capitalize"
-                        >
-                        <small>{item.Subheading}</small>
-                        </CardSubtitle>
-                    </CardBody>
-                    <CardFooter
-                        className="text-success text-right"
-                    >
-                    ${item.Price.toString().replace(/(\d+)(?=(\d{3}))/g, '$1,')}
-                    </CardFooter>
-                </Card>
-          </CarouselItem>
-        );
-      });
-
     return (
         <Carousel
-            interval={false}
+            interval={false} //disables autoslide
             activeIndex={activeIndex}
             next={next}
             previous={previous}
             >
             <CarouselIndicators
-                items={props.data}
+                items={cardSets}
                 activeIndex={activeIndex}
                 onClickHandler={goToIndex}
                 />
-            {cards}
+                {/* <Row> */}
+                {cards}
+                {/* </Row> */}
             <CarouselControl
                 direction="prev"
                 directionText="Previous"
