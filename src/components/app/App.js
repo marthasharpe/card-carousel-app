7fa51dc77608fc203e25abb3d4a9e321df21ddd4
Martha Sharpe <marthasharpe2020@gmail.com>
2019-11-23 21:39:54 -0500

    Card Carousel is finished

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import { Container } from 'reactstrap';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faImage } from '@fortawesome/free-solid-svg-icons';
-import Pages from './../pages/Pages';
-// import Cards from './../cards/Cards';
-// import Carousel from '../cardCarousel/CardCarousel'
+import CardCarousel from '../cardCarousel/CardCarousel'
 import Sort from './../sort/Sort';
 import './App.scss';
-
-library.add(faImage);
 
 const App = () => {
 
   return (
     <Container fluid>
       <Sort />
-      <Pages />
-      {/* <Carousel /> */}
+      <CardCarousel />
     </Container>
   );
 }
--- src/components/cardCarousel/CardCarousel.js
+++ src/components/cardCarousel/CardCarousel.js
@@ -1,11 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
 import {
-    // Row,
-    // Col,
-    Carousel,
-    CarouselItem,
-    CarouselControl,
-    CarouselIndicators,
     Card,
     CardBody,
     CardTitle,
@@ -13,96 +8,65 @@ import {
     CardImg,
     CardFooter,
 } from 'reactstrap';
-import { connect } from 'react-redux';
-import './CardCarousel.scss';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import './CardCarousel';
 
+const responsive = {
+    superLargeDesktop: {
+        breakpoint: { max: 4000, min: 3000 },
+        items: 6,
+        slidesToSlide: 6
+    },
+    desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 4,
+        slidesToSlide: 4
+    },
+    tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 2,
+        slidesToSlide: 2
+    },
+    mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1,
+        slidesToSlide: 1
+    },
+};
 
 const CardCarousel = (props) => {
-    const [activeIndex, setActiveIndex] = useState(0);
-    const [animating, setAnimating] = useState(false);
 
-    // // split data into data sets
-    // let dataSets = [];   
-    // for (let i = 0; i < props.data.length; i += 3) {
-    //     let dataSet = props.data.slice(i, i + 3);
-    //     dataSets.push(dataSet);
-    // }
-    // console.log(dataSets)
-    
-    // map data onto cards
-    const items = props.data.map(obj => {
+    const cards = props.data.map(obj => {
         return (
-            <CarouselItem
-                onExiting={() => setAnimating(true)}
-                onExited={() => setAnimating(false)}
-                key={obj.Heading}
-            >
-                <Card className="shadow rounded">
-                    {/* display image if in json data */}
-                    {obj.showBridge ? 
-                        <CardImg top height="150px" width="100%" src="https://ih0.redbubble.net/image.9487584.0600/pp,550x550.jpg" alt="golden gate bridge"/>
-                        :
-                        <CardImg top height="150px" width="100%" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbmDSPoVhPaWkpmlsCFKh4HTbEjnobbuks8SJBAkUA7a7RZEGr&s" alt="black background"/>}
-                    <CardBody>
-                        <CardTitle className="text-capitalize">
-                            {obj.Heading}
-                        </CardTitle>
-                        <CardSubtitle className="text-capitalize">
-                            <small>{obj.Subheading}</small>
-                        </CardSubtitle>
-                    </CardBody>
-                    <CardFooter className="text-success text-right">
-                        ${obj.Price.toString().replace(/(\d+)(?=(\d{3}))/g, '$1,')}
-                    </CardFooter>
-                </Card>
-            </CarouselItem>
-        );
-    });
-
-    const next = () => {
-        if (animating) return;
-        const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
-        setActiveIndex(nextIndex);
-    }
-
-    const previous = () => {
-        if (animating) return;
-        const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
-        setActiveIndex(nextIndex);
-    }
-
-    const goToIndex = (newIndex) => {
-        if (animating) return;
-        setActiveIndex(newIndex);
-    }
-
+            <Card className="shadow rounded"
+                style={{width: 240, height: 340}}
+                key={obj.Heading}>
+                {/* display image if in json data */}
+                {obj.showBridge ? 
+                    <CardImg top height="150px" width="100%" src="https://ih0.redbubble.net/image.9487584.0600/pp,550x550.jpg" alt="golden gate bridge"/>
+                    :
+                    <CardImg top height="150px" width="100%" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbmDSPoVhPaWkpmlsCFKh4HTbEjnobbuks8SJBAkUA7a7RZEGr&s" alt="black background"/>}
+                <CardBody>
+                    <CardTitle className="text-capitalize">
+                        {obj.Heading}
+                    </CardTitle>
+                    <CardSubtitle className="text-capitalize">
+                        <small>{obj.Subheading}</small>
+                    </CardSubtitle>
+                </CardBody>
+                <CardFooter className="text-success text-right">
+                    ${obj.Price.toString().replace(/(\d+)(?=(\d{3}))/g, '$1,')}
+                </CardFooter>
+            </Card>
+        )
+    })
+    
     return (
-        <Carousel
-            interval={false} //disables autoslide
-            activeIndex={activeIndex}
-            next={next}
-            previous={previous}
-            >
-            <CarouselIndicators
-                items={items}
-                activeIndex={activeIndex}
-                onClickHandler={goToIndex}
-                />
-                {/* <Row> */}
-                {items}
-                {/* </Row> */}
-            <CarouselControl
-                direction="prev"
-                directionText="Previous"
-                onClickHandler={previous}
-                />
-            <CarouselControl
-                direction="next"
-                directionText="Next"
-                onClickHandler={next}
-                />
+        <Carousel responsive={responsive}>
+            {cards}
         </Carousel>
-    );
+  )
 }
 
 const mapStateToProps = ({ data }) => ({
